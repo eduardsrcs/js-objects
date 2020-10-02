ce0f66c30e36213088f3d9f73e74e8b738ec97cc
eduardsrcs <itbeard@inbox.lv>
2020-10-02 19:41:58 +0300

    added modal

--- js/jq.js
+++ js/jq.js
@@ -1,13 +1,17 @@
+const modal = document.getElementById('modal')
+
 $(document).ready(function(){
   // console.log($('#but1'))
   $('#but1').click(function(){
     console.log('I\'m clicked by jQuery')
+    $('#text1').toggle(700)
   })
 })
 
 // onclick event from native js
 document.getElementById('but2').onclick = function(){
   console.log('Onclick event')
+  modal.classList.toggle('showed')
 }
 
 // addEventListener from native js
