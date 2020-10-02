028534c926e7c889e6ea07c3bf03d1025139338a
eduardsrcs <itbeard@inbox.lv>
2020-10-02 18:47:05 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const obj = {
+  name: 'Ann',
+  surname: 'Hendricks',
+  age: 23,
+  location: 'London'
+}
+
+console.log(Object.keys(obj))
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
